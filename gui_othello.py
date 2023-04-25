6e163c80a1e744f925d25c81405e83be039d4267
samazakii <zaki.s@northeastern.edu>
2023-04-25 14:40:16 -0400

    gui for manual play done

--- gui_othello.py
+++ gui_othello.py
@@ -22,31 +22,24 @@ class OthelloGUI:
         for r in range(self.game.board_size):
             board_row = []
             for c in range(self.game.board_size):
-                disc = tk.PhotoImage(file="images/green.gif")
-                if self.board[r][c] == Player.Black:
-                    disc = tk.PhotoImage(file="images/black.gif")
-                elif self.board[r][c] == Player.White:
-                    disc = tk.PhotoImage(file="images/white.gif")
-                slot = tk.Button(self.gui, image=disc, highlightbackground='#228C22', height=60, width=60, command=lambda row=r, col=c: self.button_clicked(row, col))
-                slot.pack()
-
-                # slot = tk.Button(self.gui, height=4, width=4, highlightbackground='#228C22',
-                #                  command=lambda row=r, col=c: self.button_clicked(row, col))
+                slot = tk.Button(self.gui, highlightbackground="#20693D",
+                                 height=4, width=5,
+                                 command=lambda row=r, col=c: self.button_clicked(row, col))
                 slot.grid(row=r, column=c)
                 board_row.append(slot)
             self.board_gui.append(board_row)
 
-        # self.update_board()
+        self.update_board()
 
     def update_board(self):
         for i in range(self.game.board_size):
             for j in range(self.game.board_size):
                 if self.board[i][j] == Player.Black:
-                    self.board_gui[i][j].config(text='B', bg='black')
+                    self.board_gui[i][j].config(text='B', highlightbackground='#221F20')
                 elif self.board[i][j] == Player.White:
-                    self.board_gui[i][j].config(text='W', bg='white')
+                    self.board_gui[i][j].config(text='W', highlightbackground='#FFFFFF')
                 else:
-                    self.board_gui[i][j].config(text='_', bg='green')
+                    self.board_gui[i][j].config(text='_', highlightbackground='#20693D')
 
     def button_clicked(self, row, col):
         if self.game.is_valid_move(self.board, self.current_player, row, col):
@@ -55,7 +48,10 @@ class OthelloGUI:
             if self.game.game_over(self.board):
                 self.end_game()
             else:
+                print("switched")
                 self.current_player = self.game.switch_player(self.current_player)
+        if len(self.game.get_all_valid_moves(self.board, self.current_player)) == 0:
+            self.current_player = self.game.switch_player(self.current_player)
 
     def end_game(self):
         black_count, white_count = self.game.count_pieces(self.board)
